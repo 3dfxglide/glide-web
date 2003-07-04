fc8854127fedaa525eb8861660278abd6131de96
guillemj <>
2003-07-04 01:34:23 +0000

    Initial Glide Web import.

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+/*
+ * Slightly modified web page for Glide project
+ *
+ * $Id$
+ *
+ */
+
+$headers = getallheaders();
+
+?>
+
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+  <title>Glide Open Source Project</title>
+  <link rel="stylesheet" href="http://www.sf.net/sourceforge.css" type="text/css" />
+</head>
+<body bgcolor="#FFFFFF" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" marginheight="0" marginwidth="0">
+  <!-- Main Page Stuff -->
+
+  <br />
+
+  <table width="100%" border="0" cellspacing="0" cellpadding="2" bgcolor="#FFFFFF" align="center">
+    <tr>
+      <td>
+	<center>
+	  <h1>Welcome to the Glide Open Source Project</h1>
+	  <p>
+	    We haven't put much material up yet. Please check back here for
+	    the official rollout on December 1st.
+	  </p>
+	</center>
+	<p>
+	  In the mean time here's a few links to get you started: <br />
+	  <ul>
+	    <li><a href="http://www.sf.net/project/glide">The
+	        Glide Project on SourceForge</a></li>
+	    <li><a href="http://www.3dfx.com">The 3dfx website</a></li>
+	    <li><a href="http://www.precisioninsight.com">The Precision Insight
+	        website</a></li>
+	    <li><a href="http://www.3dfxgamers.com/view.asp?PAGE=V3Drivers">
+	        Current 3dfx drivers</a></li>
+	    <li><a href="news://news.3dfx.com/3dfx.glide.linux">The 3dfx
+	        newsgroup about Linux Glide</a></li>
+	  </ul>
+	</p>
+      </td>
+    </tr>
+  </table>
+
+  <br />
+
+  <!-- Top strip -->
+  <table width="100%" border="0" cellspacing="0" cellpadding="2">
+    <tr bgcolor="#000000">
+      <tr>
+	<span class="maintitlebar">&nbsp;&nbsp;
+	  <a class="maintitlebar" href="http://sf.net/about.php">
+	    <b>About SourceForge</b></a> |
+	  <a class="maintitlebar" href="http://sf.net/contact.php">
+	    <b>Contact SourceForge</b></a>
+	</span>
+      </td>
+    </tr>
+  </table>
+  <!-- End top strip -->
+
+  <!-- Top title table -->
+  <table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="" valign="center">
+    <tr>
+      <td background="http://sf.net/images/binary-code.gif">
+	<a href="http://www.valinux.com/">
+	  <img src="http://sf.net/images/va-btn-small-light.png" align="right" alt="VA Linux Systems" hspace="5" vspace="7" border=0 width="136" height="40" /> </a>
+	<a href="http://www.sf.net/">
+	  <img src="http://sf.net/images/anvil.gif" vspace="5" hspace="5" border=0 width="97" height="75" />
+	  <img src="http://sf.net/images/sourceforge.gif" alt="SourceForge" vspace="10" hspace="0" border=0 width="227" height="70" /></a>
+      </td>
+    </tr>
+  </table>
+  <!-- End top title table -->
+
+  <!-- Footer table -->
+  <table width="100%" border="0" cellspacing="0" cellpadding="2" bgcolor="#000000">
+    <tr>
+      <td align="center">
+        <font color="#ffffff">
+	  <span class="titlebar">
+	    SourceForge is copyright (c)1999 VA Linux Systems. While in beta,
+	    all rights are reserved.
+	  </span>
+	</font>
+      </td>
+    </tr>
+  </table>
+  <!-- End footer table -->
+
+</html>
+
