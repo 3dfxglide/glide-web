fc8ac6440eeeb8b35490cc999ce88589352e68d2
guillemj <>
2003-11-28 22:19:50 +0000

    Added a TODO list.

---
+++ TODO.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * Glide project TODO list web page
+ *
+ * $Id$
+ *
+ */
+
+$headers = getallheaders();
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+  <meta http-equiv="Content-Language" content="en-us" />
+  <title>Glide Open Source Project - TODO list</title>
+  <meta name="Language" content="English" />
+  <link rel="stylesheet" type="text/css" href="glide.css" />
+</head>
+<body>
+  <!-- Main Page Stuff -->
+  <div class="content">
+    <center>
+      <h1>Glide Project TODO list</h1>
+    </center>
+
+    <p>
+      <ul>
+        <li>Make SLI work on Linux, FreeBSD.</li>
+        <li>Fix procedural texture rendering for multichip cards.</li>
+        <li>Refine SSE, SSE2 optimizations. </li>
+        <li>Port the current DOS, Win32 only functions to other platforms.</li>
+        <li>Add support for Voodoo, Voodoo2, Voodoo Rush.</li>
+        <li>Fix autothings. <i>(in progress)</i></li>
+        <li>Integrate 64 bit support patches. <i>(in progress)</i></li>
+        <li>Merge glide3 arches cvg, h3 and h5. <i>(in progress)</i></li>
+        <li>Merge glide2 and glide3.</li>
+      </ul>
+    </p>
+  </div>
+
+  <!-- Start SourceForge Footer -->
+  <div class="footer">
+    <div class="footer-top">
+      <span class="maintitlebar">&nbsp;&nbsp;
+        <a class="maintitlebar" href="http://sf.net/about.php">
+          <b>About SourceForge</b></a> |
+        <a class="maintitlebar" href="http://sf.net/contact.php">
+          <b>Contact SourceForge</b></a>
+      </span>
+    </div>
+
+    <div class="footer-content">
+      <a href="http://sf.net/">
+        <img src="http://sf.net/sflogo.php?group_id=369&amp;type=5" width="210" height="62" border="0" alt="SourceForge.net Logo" /></a>
+    </div>
+
+    <div class="footer-bottom">
+      <span class="titlebar">
+        SourceForge is copyright (c)1999 VA Linux Systems. While in beta,
+        all rights are reserved.
+      </span>
+    </div>
+  </div>
+  <!-- End SourceForge Footer -->
+
+</html>
+
